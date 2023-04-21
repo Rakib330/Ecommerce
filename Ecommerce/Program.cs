fa11d8ca7f8e1e58f0feeb2959e5711a0e094384
MD Rakibul Hasan <59144192+Rakib330@users.noreply.github.com>
2023-04-21 12:02:01 +0600

    area added

--- Ecommerce/Program.cs
+++ Ecommerce/Program.cs
@@ -32,8 +32,9 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "areas",
+    pattern: "{area:Customer}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
